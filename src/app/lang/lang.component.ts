f4a92cfdc532c7b824f50c8022a161c5a1bce2f9
nowRick <alamajjkin@rambler.ru>
2020-11-17 00:10:14 +0300

    V-01

---
+++ src/app/lang/lang.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lang',
+  templateUrl: './lang.component.html',
+  styleUrls: ['./lang.component.scss']
+})
+export class LangComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
